5e146c3ca60154c1b6805816dc253871aad8fb43
Nahuel Pelli <nahuelpelli91@gmail.com>
2023-07-17 00:26:34 -0300

    Agrego modificacion sobre el tp integrador

--- tp_integrador_enunciado/utils/data_manipulation.py
+++ tp_integrador_enunciado/utils/data_manipulation.py
@@ -1,72 +1,83 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 
 def generate_outliers(
     df: pd.DataFrame,
-    cols: list = None,
-    extreme_outlier: bool = False,
-    two_tailed: bool = False,
-    percentage: float = 0.02,
-) -> pd.DataFrame:
-    """Con esta función vamos a poder generar outliers en ciertas columnas de nuestro
-    dataset. Si le damos True a _extreme_outlier_ va a generar outliers con mucho
-    peso en la regresión (puede ser bilateral o unilateral segun _two_tailed_)
+    columns: list = None,
+    percentage: float = 0.01,
+    extreme_outliers: bool = False,
+    only_tails: bool = False,
+    two_tailed: bool = True,
+):
     """
-    seeds = np.random.randint(100, size=len(df))
+    Generate outliers in the distribution of a DataFrame.
 
-    nsamples = np.math.floor(len(df) * percentage)
-    idx_to_change = df.sample(n=nsamples).index
+    Parameters:
+    df (DataFrame): Input DataFrame containing features and one output.
+    percentage (float): Percentage of total data points that should be replaced with outliers.
+    extreme_outliers (bool): If True, it generates points with a big leverage.
+    only_tails (bool): If True, generates outliers only in the tails (close to the min and max of the feature values).
+    two_tailed (bool): If True, generates outliers from both parts of the feature (both in the min and the max).
 
-    cols = df.columns.tolist() if cols is None else cols
+    Returns:
+    DataFrame: DataFrame with the newly generated outliers.
+    """
 
-    result = df.copy(deep=True)
+    outlier_df = df.copy()
 
-    for i, col_name in enumerate(cols):
-        np.random.seed(seeds[i])
+    if columns is None:
+        columns = df.drop("target", axis=1).columns
 
-        iqr = result[col_name].quantile(0.75) - result[col_name].quantile(0.25)
+    for column in columns:
+        # Set the amount of leverage for the outliers
+        leverage = 3 if extreme_outliers else 1.5
 
-        lb = result[col_name].quantile(0.25) - 1 * iqr
-        ub = result[col_name].quantile(0.75) + 1 * iqr
+        # Calculate the IQR of the column
+        Q1 = df[column].quantile(0.25)
+        Q3 = df[column].quantile(0.75)
+        IQR = Q3 - Q1
 
-        if two_tailed:
-            outs = result[col_name].loc[
-                (result[col_name] < lb) | (result[col_name] > ub)
-            ]
+        # Set the lower and upper bounds for outliers
+        lower_bound = Q1 - (IQR * leverage)
+        upper_bound = Q3 + (IQR * leverage)
+
+        # Get the total number of outliers to be introduced
+        num_outliers = int(df.shape[0] * percentage)
+
+        # Depending on the settings, generate outliers at different places in the distribution
+        if only_tails:
+            if two_tailed:
+                # Generate outliers in the lower and upper tails
+                lower_indices = np.random.choice(
+                    df[df[column] < (Q1 - (IQR))].index,
+                    size=num_outliers // 2,
+                    replace=True,
+                )
+                upper_indices = np.random.choice(
+                    df[df[column] > (Q3 + (IQR))].index,
+                    size=num_outliers // 2,
+                    replace=True,
+                )
+                outlier_df.loc[lower_indices, column] = np.random.uniform(
+                    lower_bound, Q1, size=num_outliers // 2
+                )
+                outlier_df.loc[upper_indices, column] = np.random.uniform(
+                    Q3, upper_bound, size=num_outliers // 2
+                )
+            else:
+                # Generate outliers in the upper tail
+                upper_indices = np.random.choice(
+                    df[df[column] > upper_bound].index, size=num_outliers, replace=True
+                )
+                outlier_df.loc[upper_indices, column] = np.random.uniform(
+                    upper_bound, df[column].max(), size=num_outliers
+                )
         else:
-            outs = result[col_name].loc[(result[col_name] > ub)]
-
-        out_size = len(outs)
-        if out_size < nsamples:
-            nsamples = out_size
-
-        idx_to_change = outs.sample(nsamples, replace=False).index
-
-        if extreme_outlier:
-            outlier_sign = [
-                1 if np.random.random() < 0.9 else -1 for _ in range(nsamples)
-            ]
-
-            result[col_name].loc[idx_to_change] = np.multiply(
-                outlier_sign,
-                np.random.uniform(
-                    low=result[col_name].mean(),
-                    high=result[col_name].max() * 5,
-                    size=nsamples,
-                ),
+            # Generate outliers throughout the distribution
+            indices = np.random.choice(df.index, size=num_outliers, replace=True)
+            outlier_df.loc[indices, column] = np.random.uniform(
+                df[column].min(), df[column].max(), size=num_outliers
             )
-            result["target"].loc[idx_to_change] = np.multiply(
-                outlier_sign,
-                np.random.uniform(
-                    low=result["target"].mean(),
-                    high=result["target"].max() * 2,
-                    size=nsamples,
-                ),
-            )
-        else:
-            samples = result[col_name].loc[idx_to_change].values
-            np.random.shuffle(samples)
-            result[col_name].loc[idx_to_change] = samples
 
-    return result
+    return outlier_df

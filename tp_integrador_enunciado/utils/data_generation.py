fc113cd79394b114a808b2c3a4e1c5f87ab34fb3
Nahuel Pelli <nahuelpelli91@gmail.com>
2023-07-18 15:39:58 -0300

    Agrego correcciones sobre el tp integrador

--- tp_integrador_enunciado/utils/data_generation.py
+++ tp_integrador_enunciado/utils/data_generation.py
@@ -2,25 +2,33 @@ import numpy as np
 import pandas as pd
 
 from sklearn.datasets import make_regression
-from typing import Union
+from typing import Optional, Union
 
 def generate_dataset(n_samples: int = 1000,
                      n_features: int = 10,
                      n_informative: int = 5,
                      n_targets: int = 1,
-                     noise: float = 0.0,
-                     random_state: int = None,
+                     noise: float = 10.0,
+                     bias: float = 0.0,
+                     random_state: Optional[int] = None,
+                     effective_rank: Optional[int] = None,
                      output: str = 'dataframe') -> Union[pd.DataFrame,np.ndarray]:
     """
     Generate a regression dataset.
     The input parameters are the same as sklearn.datasets.make_regression
     """
-    X, y = make_regression(n_samples=n_samples,
+
+    X, y, _ = make_regression(n_samples=n_samples,
                            n_features=n_features,
                            n_informative=n_informative,
                            n_targets=n_targets,
                            noise=noise,
+                           effective_rank= effective_rank,
+                           bias=bias,
+                           coef=True,
+                           shuffle = False,
                            random_state=random_state)
+    
     if output == 'dataframe':
         df = pd.DataFrame(X, columns=[f'x{i}' for i in range(n_features)])
         df['target'] = y
